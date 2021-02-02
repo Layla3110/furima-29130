bae67fa043036d5603b367209da2dc784ac728e4
Layla3110 <ats.cts.0127@gmail.com>
2021-02-02 18:08:49 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,22 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :set_item, only: [:edit, :update,]
+  before_action :set_item, only: [:edit, :update, :show]
 
     def index
       @items = Item.all.order("created_at ASC")
     end
 
-    # def edit
-    # end
+    def edit
+    end
   
-    # def update
-    #   @item.update(item_params)
-    #   redirect_to root_path
-    # end
+    def update
+      @item.update(item_params)
+      if @item.valid?
+        redirect_to item_path
+      else
+        render :edit
+      end
+    end
   
     def new
       @item = Item.new
